b2d41bd10243b1b058b877507500ccbc05b1dcac
Andrea Laura <andrea_carol00@hotmail.com>
2024-02-06 14:39:53 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BlurhashImg from "./components/Image/BlurhashImg";
+import "./style.css";
 
-function App() {
+const IMG_SRC = "https://ychef.files.bbci.co.uk/3840x2160/p0b926m6.jpg";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="blurhash-wrapper">
+      <BlurhashImg src={IMG_SRC} />
     </div>
   );
 }
-
-export default App;
