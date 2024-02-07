020c2f919e7ae6a896e252897ad1df727406871a
Andrea Laura <andrea_carol00@hotmail.com>
2024-02-07 11:20:11 -0300

    add imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import BlurhashImg from "./components/Image/BlurhashImg";
 import "./style.css";
 
-const IMG_SRC = "https://ychef.files.bbci.co.uk/3840x2160/p0b926m6.jpg";
+const IMG_SRC =
+  "https://lh3.googleusercontent.com/05JfZ1ZdyzrRNvhJosUFdcjjJRFE7k2KhmeM2ujqeCbrcrCb1hkq7O_JdUBpQ3r9hi0YeSn4WgmKx3Ai8LHdM2SucxSzl9TRZ4fCAqETJ6WtHgE=w1440-e365";
 
 export default function App() {
   return (
